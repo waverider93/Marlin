5088c317f6404923e73b5fa26a559de64b960384
Vert <vert@vertshobbies.com>
2020-03-09 17:27:07 -0400

    clean up

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1877,12 +1877,14 @@
 #endif
 
 // Add a menu item to move between bed corners for manual bed adjustment
+#if DISABLED (BEAR)
 #define LEVEL_BED_CORNERS
 #if ENABLED(LEVEL_BED_CORNERS)
   #define LEVEL_CORNERS_INSET_LFRB { 30, 30, 30, 30 } // (mm) Left, Front, Right, Back insets
   #define LEVEL_CORNERS_HEIGHT      0.0   // (mm) Z height of nozzle at leveling points
   #define LEVEL_CORNERS_Z_HOP       5.0   // (mm) Z height of nozzle between leveling points
   #define LEVEL_CENTER_TOO              // Move to the center after the last corner
+#endif
 #endif
 
 /**
